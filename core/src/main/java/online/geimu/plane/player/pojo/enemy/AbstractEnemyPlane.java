3407f7ca66c15f4180f7a86226cca6f328706abe
liutaoyxz <18640076373@163.com>
2017-01-10 18:45:53 +0800

    添加测试敌机

--- core/src/main/java/online/geimu/plane/player/pojo/enemy/AbstractEnemyPlane.java
+++ core/src/main/java/online/geimu/plane/player/pojo/enemy/AbstractEnemyPlane.java
@@ -1,7 +1,6 @@
 package online.geimu.plane.player.pojo.enemy;
 
 import online.geimu.plane.player.pojo.IndependentObj;
-import online.geimu.plane.player.pojo.Position;
 
 /**
  * @author liutao
@@ -11,35 +10,8 @@ import online.geimu.plane.player.pojo.Position;
 public abstract class AbstractEnemyPlane implements IndependentObj{
 
 
-    protected int x;
-
-    protected int y;
-
-
-
-    /**
-     * @return 飞机的位置对象
-     */
-    @Override
-    public final Position getPosition() {
-        return new Position(x,y);
-    }
 
     protected abstract void debut();
 
-    public int getX() {
-        return x;
-    }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
 }
--- core/src/main/java/online/geimu/plane/player/pojo/enemy/TestEnemyPlane.java
+++ core/src/main/java/online/geimu/plane/player/pojo/enemy/TestEnemyPlane.java
@@ -1,10 +1,13 @@
 package online.geimu.plane.player.pojo.enemy;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import online.geimu.plane.player.pojo.Obj;
 import online.geimu.plane.player.pojo.Position;
 import online.geimu.plane.player.pojo.map.TestMap;
 
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * @author liutao
@@ -20,11 +23,29 @@ public class TestEnemyPlane extends AbstractEnemyPlane {
 
     private int speed;
 
+    private int x;
+
+    private int y;
+
+    private final ScheduledExecutorService works = Executors.newScheduledThreadPool(2);
+
     private volatile boolean isDebut;
 
     private Position targetPosition;
 
     private TestMap map;
+
+    private TestEnemyPlane(int x,int y,int speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public static TestEnemyPlane newInstance(int x,int y,int speed){
+        TestEnemyPlane instance = new TestEnemyPlane(x,y,speed);
+        instance.initAutoMove();
+        return instance;
+    }
 
     @Override
     public Obj checkCollision() {
@@ -46,7 +67,9 @@ public class TestEnemyPlane extends AbstractEnemyPlane {
         }else if(ty < y) {
             y = Math.max(y-speed,ty);
         }
-
+        Position now = new Position(x,y);
+        if (now.equals(targetPosition))
+            this.refreshTargetPosition();
         return false;
     }
 
@@ -58,7 +81,7 @@ public class TestEnemyPlane extends AbstractEnemyPlane {
     /**
      * 刷新目标位置
      */
-    private void refreshPosition(){
+    private void refreshTargetPosition(){
         final int maxX = map.getX();
         final int maxY = map.getY()/2;
         Random random = new Random();
@@ -67,5 +90,39 @@ public class TestEnemyPlane extends AbstractEnemyPlane {
         this.targetPosition = new Position(rx,ry);
     }
 
+    @JSONField(serialize = false)
+    @Override
+    public Position getPosition() {
+        return new Position(x,y);
+    }
 
+    /**
+     * 初始化并且自动移动
+     */
+    private void initAutoMove(){
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
